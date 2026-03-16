10fd2895a077c78d9a689e90bdcaa95055d48b5c
Елена Васильева <lena81066@gmail.com>
2026-03-16 23:11:19 +0300

    Add Note class

---
+++ ManagingNotes/ManagingNotes/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Note
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime Date { get; set; }
+
+    // Конструктор для новых заметок (дата = сейчас)
+    public Note(string title, string content)
+    {
+        Title = title;
+        Content = content;
+        Date = DateTime.Now;
+    }
+
+    // Конструктор для загрузки из файла (с указанной датой)
+    public Note(string title, string content, DateTime date)
+    {
+        Title = title;
+        Content = content;
+        Date = date;
+    }
+}
